ace969cd880f13650a3cd3233609d45190572633
Егор Денисов <deneg99@gmail.com>
2023-11-26 02:48:42 +0300

    Добавил ccsds-epp encapsulation

---
+++ ccsds/epp/include/ant-lib/ccsds-epp.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "stdint.h"
+#include "ant-lib/ehas.h"
+
+constexpr uint8_t CCSDS_EPP_PVN = 7;
+constexpr uint8_t CCSDS_EPP_EPID = 0;
+
+enum
+{
+    CCSDS_EPP_OK = 0,
+    CCSDS_EPP_ERROR_NullPtr,
+    CCSDS_EPP_ERROR_SizeTooBig,
+    CCSDS_EPP_ERROR_LenghtTooSmall,
+    CCSDS_EPP_ERROR_SizeTooSmall,
+    CCSDS_EPP_NumberOfTypes,
+};
+
+typedef enum
+{
+    CCSDS_EPP_LENOFLEN_00 = 0,
+    CCSDS_EPP_LENOFLEN_01 = 1,
+    CCSDS_EPP_LENOFLEN_10 = 2,
+    CCSDS_EPP_LENOFLEN_11 = 3,
+    CCSDS_EPP_LENOFLEN_NumberOfTypes,
+} ccsds_epp_lenoflen_t;
+
+constexpr uint8_t ccsds_epp_header_len[CCSDS_EPP_LENOFLEN_NumberOfTypes] = { 1, 2, 4, 8 };
+constexpr uint32_t ccsds_epp_max_data_len[CCSDS_EPP_LENOFLEN_NumberOfTypes] = { 0, 0xFD, 0xFFFB, 0xFFFFFFF7 };
+
+typedef struct
+{
+    uint8_t lenoflen : 2;
+    uint8_t epid: 3;
+    uint8_t pvn: 3;
+} ccsds_epp_common_header_bf_t;
+
+typedef union
+{
+    ccsds_epp_common_header_bf_t bf;
+    uint8_t byte;
+} ccsds_epp_common_header_t;
+
+typedef struct
+{
+    uint8_t size;
+} ccsds_epp_header01_t;
+
+typedef struct
+{
+    uint8_t udf_epidext;
+    uint16_t size;
+} ccsds_epp_header10_t;
+
+typedef struct
+{
+    uint8_t udf_epidext;
+    uint16_t cdf;
+    uint32_t size;
+} ccsds_epp_header11_t;
+
+class CcsdsEpp
+{
+    public:
+        CcsdsEpp(ccsds_epp_lenoflen_t lenoflen_enc):
+            _lenoflen(lenoflen_enc) {
+                EHAS_INIT_NOINIT_DEVICE("CcsdsEpp");
+                EHAS_INIT_PACK(CCSDS_EPP_ERROR, NullPtr, EHAS_ERROR);
+                EHAS_INIT_PACK(CCSDS_EPP_ERROR, SizeTooBig, EHAS_ERROR);
+                EHAS_INIT_PACK(CCSDS_EPP_ERROR, LenghtTooSmall, EHAS_ERROR);
+                EHAS_INIT_PACK(CCSDS_EPP_ERROR, SizeTooSmall, EHAS_ERROR);
+            };
+
+        int encapsulate(uint8_t* data, uint32_t size, uint8_t* buffer, uint32_t lenght);
+        int deencapsulate(uint8_t* data, uint32_t size, uint8_t* buffer, uint32_t lenght);
+
+    private:
+        const ccsds_epp_lenoflen_t _lenoflen;
+        EhasPack<CCSDS_EPP_NumberOfTypes> ehas;
+};
---
+++ ccsds/epp/src/ccsds-epp.cpp
@@ -0,0 +1,81 @@
+#include "ant-lib/ccsds-epp.h"
+
+int CcsdsEpp::encapsulate(uint8_t* data, uint32_t size, uint8_t* buffer, uint32_t lenght)
+{
+    if (!data || !buffer) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_NullPtr);
+    }
+
+    if (size > ccsds_epp_max_data_len[this->_lenoflen]) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_SizeTooBig);
+    }
+
+    uint32_t packet_size = size + ccsds_epp_header_len[this->_lenoflen];
+    if (lenght < packet_size) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_LenghtTooSmall);
+    }
+
+    ccsds_epp_common_header_t common_header = {0};
+    common_header.bf.pvn =  CCSDS_EPP_PVN;
+    common_header.bf.epid = CCSDS_EPP_EPID;
+    common_header.bf.lenoflen = this->_lenoflen;
+    memcpy(buffer, &(common_header.byte), sizeof(uint8_t));
+
+    uint8_t* header_ptr = buffer + sizeof(uint8_t);
+    switch (this->_lenoflen)
+    {
+        case CCSDS_EPP_LENOFLEN_01: {
+            ccsds_epp_header01_t header = {0};
+            header.size = packet_size;
+            memcpy(header_ptr, &header, sizeof(header));
+        } break;
+    
+        case CCSDS_EPP_LENOFLEN_10: {
+            ccsds_epp_header10_t header = {0};
+            header.size = packet_size;
+            header.udf_epidext = 0;
+            memcpy(header_ptr, &header, sizeof(header));
+        } break;
+
+        case CCSDS_EPP_LENOFLEN_11: {
+            ccsds_epp_header11_t header = {0};
+            header.size = packet_size;
+            header.udf_epidext = 0;
+            header.cdf = 0;
+            memcpy(header_ptr, &header, sizeof(header));
+        } break;
+
+        default:
+            break;
+    }
+
+    uint8_t* data_field_ptr = buffer + ccsds_epp_header_len[this->_lenoflen];
+    memcpy(data_field_ptr, data, size);
+    return packet_size;
+}
+
+int CcsdsEpp::deencapsulate(uint8_t* data, uint32_t size, uint8_t* buffer, uint32_t lenght)
+{
+    if (!data || !buffer) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_NullPtr);
+    }
+
+    if (size < sizeof(uint8_t)) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_SizeTooSmall);
+    }
+
+    ccsds_epp_common_header_t common_header = {0};
+    memcpy(&(common_header.byte), data, sizeof(uint8_t));
+    if (common_header.bf.pvn !=  CCSDS_EPP_PVN) {
+
+    }
+    if (common_header.bf.epid != CCSDS_EPP_EPID) {
+
+    }
+    ccsds_epp_lenoflen_t lenoflen = (ccsds_epp_lenoflen_t)common_header.bf.lenoflen;
+    
+    if (size < ccsds_epp_header_len[lenoflen]) {
+        EHAS_RETURN(CCSDS_EPP_ERROR_SizeTooSmall);
+    }
+
+}
